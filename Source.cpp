ca8c3c351a7375ca18f47b717cb83d8dd01429dc
solomon263 <ssolomon20031@gmail.com>
2024-03-29 12:17:14 +0000

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Favorite Recipe : [RECIPE NAME] < / title>
+< / head>
+<body>
+<!--h1 tag for the recipe name-->
+<h1>[RECIPE NAME]< / h1>
+<!--p tag for the description of the recipe-->
+<p>[DESCRIPTION OF THE RECIPE]< / p>
+<!--h2 tag for the ingredients-->
+<h2>Ingredients:< / h2>
+<!--unordered list for the ingredients-->
+<ul>
+<li>Ingredient 1 < / li >
+<li>Ingredient 2 < / li >
+<li>Ingredient 3 < / li >
+<!--Add more list items as needed for each ingredient-->
+< / ul>
+<!--h2 tag for the method-->
+<h2>Method:< / h2>
+<!--ordered list for the steps involved in the recipe method-->
+<ol>
+<li>Step 1 < / li >
+<li>Step 2 < / li >
+<li>Step 3 < / li >
+<!--Add more list items as needed for each step-->
+< / ol>
+< / body>
+< / html>
